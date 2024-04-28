42aff284658dd96751d1d4559895218879f85248
Jorge Gonzalez <jorge.gonzalez.piedra@gmail.com>
2024-04-28 13:07:40 +0200

    Initial commit

---
+++ src/main/java/com/personagen/PersonagenApplication.java
@@ -0,0 +1,13 @@
+package com.personagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonagenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personagen/PersonagenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonagenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
